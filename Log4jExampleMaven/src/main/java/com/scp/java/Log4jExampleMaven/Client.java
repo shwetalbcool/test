dd4907f77a22579ea552f1403ddd144ef2aca424
RUTURAJ <RUTURAJ@RUTURAJ-PC>
2018-12-23 11:15:16 +0530

    log4j first commit

---
+++ Log4jExampleMaven/src/main/java/com/scp/java/Log4jExampleMaven/Client.java
@@ -0,0 +1,24 @@
+package com.scp.java.Log4jExampleMaven;
+
+import org.apache.log4j.Logger;
+
+public class Client {
+
+  static Logger l = Logger.getLogger(Client.class.getName());
+
+  public static void main(String[] args) {      
+
+	  
+
+    l.debug("This is debug message");
+      l.info("This is info message");
+    l.warn("This is warn message");
+     l.fatal("This is fatal message");
+    l.error("This is error message");
+
+	  
+	
+      System.out.println("Your logic executed successfully....");
+
+  }
+}
